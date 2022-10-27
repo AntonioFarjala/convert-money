d6f3e52385e04e4b561c4b9e677043a704f426a0
Antonio Farjala <antoniofarjala@gmail.com>
2022-10-27 11:08:53 -0300

    finish project

---
+++ README.md
@@ -0,0 +1,30 @@
+<h1 align="center"> Convert Money </h1>
+
+
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=49AA26&labelColor=000000">
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="convert-money" src=".github/convert-money.png" width="100%">
+</p>
+<br>
+
+
+[ 🔗 Clique aqui para acessar](https://antoniofarjala.github.io/convert-money)
+<br>
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML e CSS
+- JavaScript
+
+
+## 💻 Projeto
+
+O DevClub Convert Money é um componente que simula um conversor de moedas, onde é possível atualizar elementos HTML via DOM.
+
---
+++ scripts.js
@@ -0,0 +1,65 @@
+const button = document.getElementById('convert-button')
+const select = document.getElementById('currency-select')
+
+const dollar = 5.2
+const euro = 5.9
+const bitcoin = 0.0000090
+
+const convertValues = () => {
+    const inputReais = document.getElementById('input-real').value
+    const realValueText = document.getElementById('real-value-text') 
+    const currencyValueText = document.getElementById('currency-value-text')
+
+      // Abaixo temos uma biblioteca que formata o numero do valor(dinheiro) para a moeda escolhida!! 
+        realValueText.innerHTML = new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+        }).format(inputReais)
+
+  
+        if (select.value === 'US$ Dólar Americano') {
+            currencyValueText.innerHTML = new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'USD',
+            }).format(inputReais / dollar)
+        }
+      
+
+        if (select.value === '€ Euro') {
+            currencyValueText.innerHTML = new Intl.NumberFormat('de-DE', {
+                style: 'currency',
+                currency: 'EUR',
+            }).format(inputReais / euro)
+        }
+        if (select.value === '₿ Bitcoin') {
+            currencyValueText.innerHTML = new Intl.NumberFormat('de-DE', {
+                style: 'currency',
+                currency: 'BTC',
+            }).format(inputReais * bitcoin)
+        }
+}
+
+currencyChange = () => {
+    const currencyName = document.getElementById('currency-name')
+    const currencyImg = document.getElementById('currency-img')
+
+    if (select.value === '€ Euro') {
+        currencyName.innerHTML = 'Euro'
+        currencyImg.src = './assets/Euro.png' 
+    }
+    if (select.value === 'US$ Dólar Americano') {
+        currencyName.innerHTML = 'Dólar Americano'
+        currencyImg.src = './assets/Usa.png'
+    }
+    if (select.value === '₿ Bitcoin') {
+        currencyName.innerHTML = 'Bitcoin'
+        currencyImg.src = './assets/btc.png'
+    }
+    convertValues()
+
+}
+
+
+button.addEventListener('click', convertValues)
+select.addEventListener('change', currencyChange)
+
