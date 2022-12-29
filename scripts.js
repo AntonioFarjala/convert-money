0ec7f73e765bb8cc6a572e9c89e7e55f011aff5d
Antonio Farjala <antoniofarjala@gmail.com>
2022-12-29 12:43:24 -0300

    update value currency BTC

--- scripts.js
+++ scripts.js
@@ -13,7 +13,7 @@ const convertValues = async () => {
 
     const dollar = data.USDBRL.high
     const euro = data.EURBRL.high
-    const bitcoin = data.BTCBRL.high * 1000           //0.000000126
+    const bitcoin = data.BTCBRL.high 
 
       // Abaixo temos uma biblioteca que formata o numero do valor(dinheiro) para a moeda escolhida!! 
         realValueText.innerHTML = new Intl.NumberFormat('pt-BR', {
